7a2f441431a17ceee49994e7fddaed00f5983467
anthonyjoyyy <13063@holbertonstudents.com>
2026-05-20 12:35:47 +0000

    sys.argv

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 
 # using command-line and argv
 
-if__name__ == "__main__":
+if __name__ == "__main__":
     import sys
 
     x = len(sys.argv) - 1
@@ -10,12 +10,12 @@ if__name__ == "__main__":
     if x == 0:
         print("0 arguments.")
 
-    elif x = 1:
-        print("1 argument.")
-    print("1: {}".format(sys.argv[1]))
+    elif x == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
 
     else:
-        print("{} arguments".format(x))
+        print("{} arguments:".format(x))
 
     for p in range(1, x + 1):
         print("{}:{}".format(p, sys.argv[p]))
