8299ae3e9b052b96adb3b3011bab19dc7ec78d01
anthonyjoyyy <13063@holbertonstudents.com>
2026-05-20 09:31:12 +0000

    fixing string format

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,11 +3,11 @@
 # importing functions and using it for addition
 
 if __name__ == "__main__":
-from add_0 import add
+    from add_0 import add
 
 a = 1
 b = 2
 
 equal = add(a, b)
 
-print(f'{a} + {b} = {equal}')
+print("{} + {} = {}".format(a, b, equal))
