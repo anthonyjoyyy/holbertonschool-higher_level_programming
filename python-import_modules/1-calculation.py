19bb43e57dd024f38ac181928c392546eecbaa80
anthonyjoyyy <13063@holbertonstudents.com>
2026-05-20 10:23:49 +0000

    adding four functions

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+# importing mathematical functions
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, div, mul
+
+    a = 10
+    b = 5
+
+    addition = add(a, b)
+    print("{} + {} = {}".format(a, b, addition))
+
+    subtraction = sub(a, b)
+    print("{} - {} = {}".format(a, b, subtraction))
+
+    multiplication = mul(a, b)
+    print("{} * {} = {}".format(a, b, multiplication))
+
+    division = div(a, b)
+    print("{} / {} = {}".format(a, b, division))
