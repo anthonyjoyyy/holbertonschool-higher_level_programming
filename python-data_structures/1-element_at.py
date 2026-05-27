368f17f7844375b86de32a8485d56ad31328a973
anthonyjoyyy <13063@holbertonstudents.com>
2026-05-27 04:56:02 +0000

    idx

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# writing a function to retrieve an element from a list
+
+def element_at(my_list, idx):
+
+    if my_list[idx] < 0:
+        return None
+
+    elif idx >= len(my_list):
+        return None
+
+    else:
+        return my_list[idx]
