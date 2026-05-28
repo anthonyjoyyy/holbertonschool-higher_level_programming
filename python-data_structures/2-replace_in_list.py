3dda9da58b9b4576c05a9fb23e04f1d8fa083a4b
anthonyjoyyy <13063@holbertonstudents.com>
2026-05-28 03:38:36 +0000

    pycodestyle

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -6,11 +6,11 @@ def replace_in_list(my_list, idx, element):
 
     my_list[idx] = "element"
 
-        if idx < 0:
-            return my_list
+    if idx < 0:
+        return my_list
 
-        elif idx >= len(my_list):
-            return my_list
+    elif idx >= len(my_list):
+        return my_list
 
-        else:
-            return my_list[idx]
+    else:
+        return my_list[idx]
