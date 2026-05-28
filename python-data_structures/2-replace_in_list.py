2d406b1dd55d37ae0af33fe6bb0f036429d67605
anthonyjoyyy <13063@holbertonstudents.com>
2026-05-28 03:48:14 +0000

    fixed code

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -4,11 +4,12 @@
 
 def replace_in_list(my_list, idx, element):
 
-    if idx < 0:
+    if idx < 0: # if the index is a negative number, return the original list
         return my_list
 
-    elif idx >= len(my_list):
+    elif idx >= len(my_list): # if the index is greater than the string
         return my_list
 
     else:
-        return my_list[idx] = element
+        my_list[idx] = element # replace the index with the new element
+        return my_list
