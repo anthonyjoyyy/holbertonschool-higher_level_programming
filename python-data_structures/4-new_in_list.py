df67a1b962988ab135e1096e42330229537fad15
anthonyjoyyy <13063@holbertonstudents.com>
2026-05-28 09:20:58 +0000

    copy list

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# copy a list and edit the copied list, then return it
+
+# function prototype
+def new_in_list(my_list, idx, element):
+
+    # checking the safeguards first
+    if idx < 0 or idx >= len(my_list):
+        return the_list = my_list.copy()
+
+# now edit the element in the copied list
+    else:
+        the_list = my_list.copy()
+        the_list[idx] = element
+        return the_list
