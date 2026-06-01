292389211c16b8a6d502fadb6052477c820ca40e
anthonyjoyyy <13063@holbertonstudents.com>
2026-06-01 04:30:44 +0000

    fixed loop

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -7,7 +7,7 @@ def divisible_by_2(my_list=[]):
     newlist = []
 
     for number in my_list:
-        if my_list[] % 2 == 0:
+        if number % 2 == 0:
             newlist.append(True)
         else:
             newlist.append(False)
