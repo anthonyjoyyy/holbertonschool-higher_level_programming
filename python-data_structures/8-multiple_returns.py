a1c65b77e05dba1bfe2c3e709e89e8be9658c34f
anthonyjoyyy <13063@holbertonstudents.com>
2026-06-01 03:09:55 +0000

    tuple length

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# returning a tuple with the length of string and first element
+
+def multiple_returns(sentence):
+
+    if len(sentence) == 0
+        return (0, None)
+
+    tup = len(sentence), sentence[0]
+
+    return tup
