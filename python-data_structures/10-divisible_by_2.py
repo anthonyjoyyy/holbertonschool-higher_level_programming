1ef7e6f61ecac32c68939baccb3219fb383e24aa
anthonyjoyyy <13063@holbertonstudents.com>
2026-06-01 04:29:14 +0000

    fixed loop

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
-# functiont that finds whether the value is divisble by 2 or not
+# function that finds whether the value is divisble by 2 or not
 
 def divisible_by_2(my_list=[]):
 
     newlist = []
 
-    if my_list[] % 2 == 0:
-        newlist.append(True)
-    else:
-        newlist.append(False)
+    for number in my_list:
+        if my_list[] % 2 == 0:
+            newlist.append(True)
+        else:
+            newlist.append(False)
 
     return newlist
