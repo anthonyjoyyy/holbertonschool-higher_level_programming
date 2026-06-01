a6b0f2cb992147f0321e2c0fcaae5f7ed89ba5df
anthonyjoyyy <13063@holbertonstudents.com>
2026-06-01 04:25:20 +0000

    modulo 2 string

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# functiont that finds whether the value is divisble by 2 or not
+
+def divisible_by_2(my_list=[]):
+    if my_list[] % 2 == 0:
+        newlist.append("True")
+    else:
+        newlist.append("False")
+
+    return newlist
