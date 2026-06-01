b01cf7a3a2592d22fdcdc5ca5ab613b0ff0fb2b1
anthonyjoyyy <13063@holbertonstudents.com>
2026-06-01 04:27:10 +0000

    fixed empty list and boolean

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,9 +3,12 @@
 # functiont that finds whether the value is divisble by 2 or not
 
 def divisible_by_2(my_list=[]):
+
+    newlist = []
+
     if my_list[] % 2 == 0:
-        newlist.append("True")
+        newlist.append(True)
     else:
-        newlist.append("False")
+        newlist.append(False)
 
     return newlist
