b9b2a5bb7a3fe752247823c979bb2fb1f6f17255
anthonyjoyyy <13063@holbertonstudents.com>
2026-06-02 02:05:45 +0000

    fixing errors

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,8 +3,8 @@
 # deleting specific element
 
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > =len(my_list):
         return my_list
 
-    del my_list(idx)
+    del my_list[idx]
     return my_list
