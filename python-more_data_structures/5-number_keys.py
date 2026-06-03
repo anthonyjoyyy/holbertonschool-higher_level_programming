bc64d09c4632f4a5a453d9c3a61b4a38b98c6248
anthonyjoyyy <13063@holbertonstudents.com>
2026-06-03 11:08:28 +0000

    number of keys in dictionary

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# counting the number of keys in a dictionary
+
+def number_keys(a_dictionary):
+    number_of_keys = len(a_dictionary)  # len() can check the number of keys
+    return number_of_keys
