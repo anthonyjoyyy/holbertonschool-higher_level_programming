45293c0b173eaf2aeaed9f58d075136df7ee9a3f
anthonyjoyyy <13063@holbertonstudents.com>
2026-06-03 10:15:21 +0000

    search and replace

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# replace all occurences of `search` with `replace`
+
+def search_replace(my_list, search, replace):
+    new_list = []
+    for x in my_list:
+        if x == search:
+            new_list.append(replace)
+
+        else:
+            new_list.append(x)
+
+    return new_list
