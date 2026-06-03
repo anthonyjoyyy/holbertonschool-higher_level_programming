ff91b33d289859af7a7268a9ec6b02889147e536
anthonyjoyyy <13063@holbertonstudents.com>
2026-06-03 11:04:14 +0000

    unique elements

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# finding out what elements are unique to either of the sets
+
+def only_diff_elements(set_1, set_2):
+    unique_elements = set_1 ^ set_2  # finds the unique operators
+
+    return unique_elements
