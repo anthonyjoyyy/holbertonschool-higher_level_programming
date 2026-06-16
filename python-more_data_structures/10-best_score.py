1ddbe65e9f8784ac8ea77acc8fc57d2dbf751b34
anthonyjoyyy <13063@holbertonstudents.com>
2026-06-16 08:19:14 +0000

    highest value

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+        
+    max_winner = None
+    highest_score = float('-inf')
+    
+    for key in a_dictionary:
+        current_score = a_dictionary[key]
+        
+        if current_score > highest_score:
+            highest_score = current_score
+            max_winner = key
+            
+    return max_winner
