bfcfaefa59a0b9aa6ca60bc6b8e246db05c0b460
anthonyjoyyy <13063@holbertonstudents.com>
2026-07-01 06:31:24 +0000

    printing integers

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# printing an integer
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+
+    except ValueError:
+        return False
