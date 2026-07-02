e46e3284527d99dd7fb9a33f8d86cdcfa2aed8c6
anthonyjoyyy <13063@holbertonstudents.com>
2026-07-02 01:45:07 +0000

    dividing 2 lists

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+# Dividing 2 lists element by element
+
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+
+    for i in range(list_length):
+        div_result = 0
+
+        try:
+            div_result = my_list_1[i] / my_list_2[i]
+
+        except ZeroDivisionError:
+            print("division by 0")
+
+        except TypeError:
+            print("wrong type")
+
+        except IndexError:
+            print("out of range")
+
+        finally:
+            new_list.append(div_result)
+
+    return new_list
