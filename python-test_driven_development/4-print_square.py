dccb0bbd524b38b1ee9409b2a743dd50cf416335
anthonyjoyyy <13063@holbertonstudents.com>
2026-07-09 21:23:50 +0000

    #square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -6,9 +6,15 @@ This module contains a function that prints the square of a number.
 
 
 def print_square(size):
+
+    """
+prints a square with the character '#'
+"""
+
     if type(size) not in [int]:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if type(size) in [float] and size < 0:
-        raise TypeError("size must be an integer")
+
+    for x in range(size):
+        print("#" * size)
