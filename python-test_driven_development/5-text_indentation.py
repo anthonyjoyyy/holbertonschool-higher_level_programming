a718e72db9dadaed23f281d1833d45223c132bfa
anthonyjoyyy <13063@holbertonstudents.com>
2026-07-10 01:50:06 +0000

    skip

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module contains a function that prints a text in a specific format.
+"""
+
+
+def text_indentation(text):
+    """
+    prints a text with 2 new lines after each of these characters: ., ? and :
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    skip = True
+
+    for char in text:
+        if skip and char == " ":
+            continue
+
+            skip = False
+
+            print(char, end="")
+
+            if char in [".", "?", ":"]:
+                print("\n\n")
+                skip = True
