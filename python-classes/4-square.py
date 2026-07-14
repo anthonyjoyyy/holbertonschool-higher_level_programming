ee7a7bfba8512d7bb8e0f8a297c2cdd72ce89c23
anthonyjoyyy <13063@holbertonstudents.com>
2026-07-14 04:37:39 +0000

    getter and setter - fixing indentation

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -12,16 +12,16 @@ class Square:
 
         self.size = size
 
-        @property
-        def size(self):
-            return self.__size
+    @property
+    def size(self):
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            if type(value) is not int:
-                raise TypeError("size must be an integer")
-            if value < 0:
-                raise ValueError("size must be >= 0")
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
 
     def area(self):
         return self.__size ** 2
