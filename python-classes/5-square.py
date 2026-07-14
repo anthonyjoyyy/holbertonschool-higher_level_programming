182a9f6f0bbeb21ded572b5bdfd519429345f273
anthonyjoyyy <13063@holbertonstudents.com>
2026-07-14 04:43:08 +0000

    task 5 trial

---
+++ python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This module contains a Square class and defines a square
+"""
+
+
+class Square:
+    """
+    This class defines a square.
+    """
+    def __init__(self, size=0):
+
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if size == 0:
+            print()
