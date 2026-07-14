9165c13caf9de7ef507c5b41a5ed00696af54d2a
anthonyjoyyy <13063@holbertonstudents.com>
2026-07-14 03:58:31 +0000

    returning area of square

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module contains a Square class and defines a square
+"""
+
+
+class Square:
+    """
+    This class defines a square, now raising exception messages.
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
