fccded1d28efe05f0bf13b5210578f046d3ab16b
anthonyjoyyy <13063@holbertonstudents.com>
2026-07-14 03:31:37 +0000

    square task 1 - instantiation and private attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+This module defines a class, Square, with a private attribute & instantiation.
+"""
+
+
+class Square:
+    """
+        This class defines a square
+        """
+    def __init__(self, size):
+        self.__size = size
