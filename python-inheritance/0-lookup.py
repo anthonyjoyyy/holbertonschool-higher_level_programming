b151a12d100a161090ebc62af0e68e7b4a796822
anthonyjoyyy <13063@holbertonstudents.com>
2026-07-25 08:08:11 +0000

    first attempt

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+modules returns the list of available attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """
+    returning lists and methods
+    """
+
+    return list
