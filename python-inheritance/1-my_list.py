0a6b610c011e6bc15c30f933f26232274a12258e
anthonyjoyyy <13063@holbertonstudents.com>
2026-07-29 07:22:21 +0000

    list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines a class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    """A custom list class that inherits from Python's built-in list."""
+
+    def print_sorted(self):
+        print(sorted(self))
