2c9a41f348d2cf8c17918185b3bf32f594a3d8ef
anthonyjoyyy <13063@holbertonstudents.com>
2026-08-01 02:15:41 +0000

    class instance obj

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+this module is called "exact same object"
+"""
+
+
+def is_same_class(obj, a_class):
+    """Returns True if obj is exactly an instance of a_class, otherwise False."""
+    return type(obj) is a_class
