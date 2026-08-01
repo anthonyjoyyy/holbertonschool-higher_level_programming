93b95593733d001dd41e51ac0557bf66967c9c19
anthonyjoyyy <13063@holbertonstudents.com>
2026-08-01 04:12:34 +0000

    exception error

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains an empty class
+"""
+
+
+class BaseGeometry:
+    """class that has instance method"""
+    def area(self):
+        """a function that raises an exception error"""
+        raise Exception("area() is not implemented")
