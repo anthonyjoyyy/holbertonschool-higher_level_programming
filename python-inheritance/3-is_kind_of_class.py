b76ca6bdb6c42e835e86ac9ce9d1cf0eefefad6c
anthonyjoyyy <13063@holbertonstudents.com>
2026-08-01 02:58:03 +0000

    3-is_kind_of_class.py

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
-This modules defines a function and checks inheritance.
+This modules defines a function and checks inherited instances
 """
 
 
-def is_kind_of_class(obj, a_class):
 
+def is_kind_of_class(obj, a_class):
+"""Checks if the object is instance of class or inherited from"""
+return isinstance(obj, a_class)
