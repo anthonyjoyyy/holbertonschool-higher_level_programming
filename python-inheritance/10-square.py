64b3032e971a3d2202ed20ffcb35040f64f0d579
anthonyjoyyy <13063@holbertonstudents.com>
2026-08-07 04:16:21 +0000

    incomplete

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """This class represents a square."""
