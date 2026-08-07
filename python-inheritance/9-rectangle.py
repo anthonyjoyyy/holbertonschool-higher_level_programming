ef4753b174b3e7b9e750713c578767667904b648
anthonyjoyyy <13063@holbertonstudents.com>
2026-08-07 04:10:50 +0000

    inherit with added defs

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Writing a module that inherits from BaseGeometry
+"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """This class represents a rectangle."""
+
+    def __init__(self, width, height):
+        """initialises the rectangle with a width and height"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the rectangle description."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
